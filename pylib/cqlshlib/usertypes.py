e0409fe3420c103ee2512104997aedd499bf1a8a
Mikhail Stepura <mishail@apache.org>
2014-02-12 20:25:07 -0800

    WIP class based

--- pylib/cqlshlib/usertypes.py
+++ pylib/cqlshlib/usertypes.py
@@ -19,62 +19,93 @@ from cql.marshal import uint16_unpack
 USER_TYPE = '\'org.apache.cassandra.db.marshal.UserType\''
 
 
-def _is_usertype(cls):
-    return cls.typename == USER_TYPE
-
-
-def _decode_ut_name(cls):
-    return cls.subtypes[1].cassname.decode("hex")
+# def _is_usertype(cls):
+#     return cls.typename == USER_TYPE
+#
+#
+# def _decode_ut_name(cls):
+#     return cls.subtypes[1].cassname.decode("hex")
 
 #/cql.cqltypes._CassandraType#cql_parameterized_type
-def cql_parameterized_type(cls):
-    """
-    Return a CQL type specifier for this type. If this type has parameters,
-    they are included in standard CQL <> notation.
-    """
-    if _is_usertype(cls):
-        return _decode_ut_name(cls)
+# def cql_parameterized_type(cls):
+#     """
+#     Return a CQL type specifier for this type. If this type has parameters,
+#     they are included in standard CQL <> notation.
+#     """
+#     if _is_usertype(cls):
+#         return _decode_ut_name(cls)
+#
+#     if not cls.subtypes:
+#         return cls.typename
+#     return '%s<%s>' % (cls.typename, ', '.join(styp.cql_parameterized_type() for styp in cls.subtypes))
 
-    if not cls.subtypes:
-        return cls.typename
-    return '%s<%s>' % (cls.typename, ', '.join(styp.cql_parameterized_type() for styp in cls.subtypes))
 
-
-def _deserialize(ut_dict_func, cls, byts):
-    ksname = cls.subtypes[0].cassname
-    utname = _decode_ut_name(cls)
-    ks_dict = ut_dict_func().get(ksname, {})
-    types_list = ks_dict.get(utname, [])
-    if not types_list:
-        return None
-
-    p = 0
-    result = []
-    for col_name, col_type in types_list:
-        if p == len(byts):
-            break
-        itemlen = uint16_unpack(byts[p:p + 2])
-        p += 2
-        item = byts[p:p + itemlen]
-        p += itemlen
-        result.append((col_name, col_type.from_binary(item)))
-        p += 1
-
-    return result
+# def _deserialize(ut_dict_func, cls, byts):
+#     ksname = cls.subtypes[0].cassname
+#     utname = _decode_ut_name(cls)
+#     ks_dict = ut_dict_func().get(ksname, {})
+#     types_list = ks_dict.get(utname, [])
+#     if not types_list:
+#         return None
+#
+#     p = 0
+#     result = []
+#     for col_name, col_type in types_list:
+#         if p == len(byts):
+#             break
+#         itemlen = uint16_unpack(byts[p:p + 2])
+#         p += 2
+#         item = byts[p:p + itemlen]
+#         p += itemlen
+#         result.append((col_name, col_type.from_binary(item)))
+#         p += 1
+#
+#     return result
 
 #cql.cqltypes._CassandraType#from_binary
-def from_binary(ut_dict_func, cls, byts):
-    """
-    Deserialize a bytestring into a value. See the deserialize() method
-    for more information. This method differs in that if None or the empty
-    string is passed in, None may be returned.
-    """
-    if byts is None:
-        return None
-    if byts == '' and not cls.empty_binary_ok:
-        return None
-    if _is_usertype(cls):
-        return _deserialize(ut_dict_func, cls, byts)
-    return cls.deserialize(byts)
+# def from_binary(ut_dict_func, cls, byts):
+#     """
+#     Deserialize a bytestring into a value. See the deserialize() method
+#     for more information. This method differs in that if None or the empty
+#     string is passed in, None may be returned.
+#     """
+#     if byts is None:
+#         return None
+#     if byts == '' and not cls.empty_binary_ok:
+#         return None
+#     if _is_usertype(cls):
+#         return _deserialize(ut_dict_func, cls, byts)
+#     return cls.deserialize(byts)
+
+from cql.cqltypes import CompositeType
+
+class UserType(CompositeType):
+    typename = 'UserType'
+
+    @classmethod
+    def apply_parameters(cls, *subtypes):
+        newname = subtypes[1].cassname.decode("hex")
+        return type(newname, (cls,), {'subtypes': subtypes[2:], 'cassname': newname, 'typename': newname})
+
+    @classmethod
+    def cql_parameterized_type(cls):
+        return cls.typename
+
+    @classmethod
+    def deserialize_safe(cls, byts):
+        p = 0
+        result = []
+        for col_name, col_type in enumerate(cls.subtypes):
+            if p == len(byts):
+                break
+            itemlen = uint16_unpack(byts[p:p + 2])
+            p += 2
+            item = byts[p:p + itemlen]
+            p += itemlen
+            result.append((col_name, col_type.from_binary(item)))
+            p += 1
+
+        return result
+
 
 
