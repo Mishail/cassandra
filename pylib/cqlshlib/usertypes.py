5d6fda50bd1d2decdc85c314b38ce5ff4b2e624d
Mikhail Stepura <mishail@apache.org>
2014-02-12 22:22:11 -0800

    Fieldnames!!!

--- pylib/cqlshlib/usertypes.py
+++ pylib/cqlshlib/usertypes.py
@@ -15,70 +15,16 @@
 # limitations under the License.
 
 from cql.marshal import uint16_unpack
-
-#USER_TYPE = '\'org.apache.cassandra.db.marshal.UserType\''
-
-
-# def _is_usertype(cls):
-#     return cls.typename == USER_TYPE
-#
-#
-# def _decode_ut_name(cls):
-#     return cls.subtypes[1].cassname.decode("hex")
-
-#/cql.cqltypes._CassandraType#cql_parameterized_type
-# def cql_parameterized_type(cls):
-#     """
-#     Return a CQL type specifier for this type. If this type has parameters,
-#     they are included in standard CQL <> notation.
-#     """
-#     if _is_usertype(cls):
-#         return _decode_ut_name(cls)
-#
-#     if not cls.subtypes:
-#         return cls.typename
-#     return '%s<%s>' % (cls.typename, ', '.join(styp.cql_parameterized_type() for styp in cls.subtypes))
-
-
-# def _deserialize(ut_dict_func, cls, byts):
-#     ksname = cls.subtypes[0].cassname
-#     utname = _decode_ut_name(cls)
-#     ks_dict = ut_dict_func().get(ksname, {})
-#     types_list = ks_dict.get(utname, [])
-#     if not types_list:
-#         return None
-#
-#     p = 0
-#     result = []
-#     for col_name, col_type in types_list:
-#         if p == len(byts):
-#             break
-#         itemlen = uint16_unpack(byts[p:p + 2])
-#         p += 2
-#         item = byts[p:p + itemlen]
-#         p += itemlen
-#         result.append((col_name, col_type.from_binary(item)))
-#         p += 1
-#
-#     return result
-
-#cql.cqltypes._CassandraType#from_binary
-# def from_binary(ut_dict_func, cls, byts):
-#     """
-#     Deserialize a bytestring into a value. See the deserialize() method
-#     for more information. This method differs in that if None or the empty
-#     string is passed in, None may be returned.
-#     """
-#     if byts is None:
-#         return None
-#     if byts == '' and not cls.empty_binary_ok:
-#         return None
-#     if _is_usertype(cls):
-#         return _deserialize(ut_dict_func, cls, byts)
-#     return cls.deserialize(byts)
-
 from cql.cqltypes import CompositeType
 from formatting import formatter_for, format_value_utype
+
+
+def get_field_names(ks_name, ut_name):
+    """
+    UserTypes will use this function to get its fields names from Shell's utypes_dict
+    """
+    raise NotImplementedError("this function shall be overloaded by Shell")
+
 
 class UserType(CompositeType):
     typename = 'UserType'
@@ -87,8 +33,11 @@ class UserType(CompositeType):
     def apply_parameters(cls, *subtypes):
         ksname = subtypes[0].cassname
         newname = subtypes[1].cassname.decode("hex")
+        field_names = get_field_names(ksname, newname)
+        assert len(field_names) == len(subtypes[2:])
         formatter_for(newname)(format_value_utype)
-        return type(newname, (cls,), {'subtypes': subtypes[2:], 'cassname': cls.cassname, 'typename': newname})
+        return type(newname, (cls,), {'subtypes': subtypes[2:],
+                                      'cassname': cls.cassname, 'typename': newname, 'fieldnames': field_names})
 
     @classmethod
     def cql_parameterized_type(cls):
@@ -98,7 +47,7 @@ class UserType(CompositeType):
     def deserialize_safe(cls, byts):
         p = 0
         result = []
-        for col_name, col_type in enumerate(cls.subtypes):
+        for col_name, col_type in zip(cls.fieldnames, cls.subtypes):
             if p == len(byts):
                 break
             itemlen = uint16_unpack(byts[p:p + 2])
