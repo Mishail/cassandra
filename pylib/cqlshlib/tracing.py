e9848dc0115c8ca41b42c450835fe97e19e37b77
Mikhail Stepura <mishail@apache.org>
2014-02-19 11:05:20 -0800

    Convert from UTC in tracing

--- pylib/cqlshlib/tracing.py
+++ pylib/cqlshlib/tracing.py
@@ -14,8 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from cassandra.cqltypes import UTF8Type, InetAddressType, Int32Type
 from cqlshlib.displaying import MAGENTA
+from datetime import datetime
+import time
 
 TRACING_KS = 'system_traces'
 SESSIONS_CF = 'sessions'
@@ -28,7 +29,7 @@ def print_trace_session(shell, session, session_id):
         return
     names = ['activity', 'timestamp', 'source', 'source_elapsed']
 
-    formatted_names = [shell.myformat_colname(name) for name in names]
+    formatted_names = map(shell.myformat_colname, names)
     formatted_values = [map(shell.myformat_value, row) for row in rows]
 
     shell.writeresult('')
@@ -51,17 +52,16 @@ def fetch_trace_session(session, session_id):
                    "FROM %s.%s "
                    "WHERE session_id = %s" % (TRACING_KS, EVENTS_CF, session_id))
 
-    rows = []
     # append header row (from sessions table).
-    #TODO: figure out UTC thing
-    rows.append([trace.request, str(trace.started_at), trace.coordinator, 0])
+    rows = [[trace.request, str(datetime_from_utc_to_local(trace.started_at)), trace.coordinator, 0]]
+
     # append main rows (from events table).
     for event in events:
         rows.append([event.activity, str(format_timeuuid(event.event_id)), event.source, event.source_elapsed])
     # append footer row (from sessions table).
     if trace.duration:
         from datetime import timedelta
-        finished_at = str(trace.started_at + timedelta(microseconds=trace.duration))
+        finished_at = str(datetime_from_utc_to_local(trace.started_at) + timedelta(microseconds=trace.duration))
     else:
         finished_at = trace.duration = "--"
 
@@ -72,4 +72,9 @@ def fetch_trace_session(session, session_id):
 def format_timeuuid(value):
     from datetime import datetime
     return datetime.fromtimestamp((value.get_time() - 0x01b21dd213814000L)*100/1e9)
+
+def datetime_from_utc_to_local(utc_datetime):
+    now_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    return utc_datetime + offset
 
